d30f48bcd1c2deb511ff89a4cc9555866f11717b
hanclang <874099925@qq.com>
2023-09-16 11:21:11 +0800

    init

---
+++ lua/essentials.lua
@@ -0,0 +1,59 @@
+local option = vim.opt
+local buffer = vim.b
+local global = vim.g
+
+-- 系统剪贴板
+option.clipboard:append("unnamedplus")
+-- Globol Settings --
+option.showmode = false
+option.backspace = { "indent", "eol", "start" }
+option.tabstop = 2
+option.shiftwidth = 2
+option.expandtab = true
+option.shiftround = true
+option.autoindent = true
+option.smartindent = true
+option.number = true
+option.relativenumber = true
+option.wildmenu = true
+option.hlsearch = false
+option.ignorecase = true
+option.smartcase = true
+option.completeopt = { "menu", "menuone" }
+option.cursorline = true
+option.termguicolors = true
+option.signcolumn = "yes"
+option.autoread = true
+option.title = true
+option.swapfile = false
+option.backup = false
+option.updatetime = 50
+option.mouse = "a"
+-- option.undofile = true
+-- option.undodir = vim.fn.expand('$HOME/.local/share/nvim/undo')
+option.exrc = true
+option.wrap = false
+option.splitright = true
+
+-- Buffer Settings --
+buffer.fileenconding = "utf-8"
+
+-- Global Settings --
+global.mapleader = " "
+
+-- indent-blankline.nvim --
+option.list = true
+option.listchars:append "space:⋅"
+option.listchars:append "eol:↴"
+-- Key mappings --
+-- vim.keymap.set({ "n", "i", "v" }, "<Left>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Right>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Up>", "<Nop>")
+-- vim.keymap.set({ "n", "i", "v" }, "<Down>", "<Nop>")
+
+-- vim.keymap.set("n", "<A-Tab>", "<cmd>bNext<CR>") 
+-- vim.keymap.set("n", "<leader>bc", "<cmd>bd<CR>")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set('n', '<C-e>', ':NvimTreeToggle<CR>')
+-- vim.keymap.set({ "v", "n" }, "<leader>y", "\"+y")
---
+++ lua/plugins/gitsigns.lua
@@ -0,0 +1,23 @@
+return {
+  -- git signs highlights text that has changed since the list
+  -- git commit, and also lets you interactively stage & unstage
+  -- hunks in a commit.
+  "lewis6991/gitsigns.nvim",
+  config = function()
+    require('gitsigns').setup({
+      signs      = {
+        add = { text = "▎" },
+        change = { text = "▎" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "~" },
+        untracked = { text = "▎" },
+      },
+      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
+    })
+  end
+
+}
